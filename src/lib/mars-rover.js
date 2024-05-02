7d8e09f8e16d5c6d2ab539a0a3b609c776904e54
ari-abendstern <ari.abendstern@gmx.com>
2024-05-02 20:17:55 +0100

    it's giving disaster

--- src/lib/mars-rover.js
+++ src/lib/mars-rover.js
@@ -1,3 +1,32 @@
-export function marsRover() {
-  return 'mars-rover';
+export function marsRover(commandString) {
+  if (!commandString) return '0:0:N';
+
+  const commands = commandString.split('');
+
+  let xCoordinate = 0;
+  let yCoordinate = 0;
+  let direction = 'N';
+
+  const navigationParser = {
+    N: { L: 'W', R: 'E' },
+    S: { L: 'E', R: 'W' },
+    E: { L: 'N', R: 'S' },
+    W: { L: 'S', R: 'N' },
+  };
+
+  commands.forEach((command) => {
+    switch (command) {
+      case 'M':
+        yCoordinate++;
+        if (yCoordinate === 10) yCoordinate = 0;
+        break;
+      case 'L':
+      case 'R':
+        direction = navigationParser[direction][command];
+        break;
+      default:
+    }
+  });
+
+  return `${xCoordinate}:${yCoordinate}:${direction}`;
 }
--- src/lib/mars-rover.spec.js
+++ src/lib/mars-rover.spec.js
@@ -1,6 +1,16 @@
 import { marsRover } from './mars-rover';
 describe('marsRover', () => {
-  it('should work', () => {
-    expect(marsRover()).toEqual('mars-rover');
+  it('should return the starting position when given no commands', () => {
+    expect(marsRover()).toEqual('0:0:N');
+  });
+  it('should return correct new position when given only M commands', () => {
+    expect(marsRover('M')).toBe('0:1:N');
+    expect(marsRover('MMM')).toBe('0:3:N');
+    expect(marsRover('MMMMMMMMMM')).toBe('0:0:N');
+  });
+  it('should return the correct new direction when given only directional commands', () => {
+    expect(marsRover('L')).toBe('0:0:W');
+    expect(marsRover('R')).toBe('0:0:E');
+    expect(marsRover('LL')).toBe('0:0:S');
   });
 });
